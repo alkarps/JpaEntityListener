2fe87eda6d0420047563edbcd31fa5f96bcb67e0
alkarps <alserkarpov@gmail.com>
2024-10-07 22:02:22 +0300

    Simple test EntityListeners

--- src/main/java/ru/alkarps/jpaentitylistener/repository/TestRepository.java
+++ src/main/java/ru/alkarps/jpaentitylistener/repository/TestRepository.java
@@ -1,8 +1,6 @@
 package ru.alkarps.jpaentitylistener.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.alkarps.jpaentitylistener.repository.entity.TestEntity;
 
@@ -10,7 +8,4 @@ import java.util.UUID;
 
 @Repository
 public interface TestRepository extends JpaRepository<TestEntity, UUID> {
-    @Modifying
-    @Query("update TestEntity set status = :newStatus where id = :id")
-    void updateBy(UUID id, TestEntity.Status newStatus);
 }
